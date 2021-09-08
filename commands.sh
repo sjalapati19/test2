ebff945d4020ec3fd5edaebb24c6ce5c4b57db2c
Davide Nastri <d.nastri@gmail.com>
2021-09-08 22:40:17 +0200

    feat: added commands.sh

---
+++ commands.sh
@@ -0,0 +1,6 @@
+git clone git@github.com:ltpitt/azure-pipeline-exercise.git
+az webapp up -n azure-pipeline-exercise -g azure-pipeline-exercise --sku 
+S1
+chmod +x make_predict_azure_app.sh && ./make_predict_azure_app.sh
+az webapp log tail -g azure-pipeline-exercise -n azure-pipeline-exercise
+az group delete -n azure-pipeline-exercise
